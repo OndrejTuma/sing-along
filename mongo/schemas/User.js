0b6dde0e4af942a6f6a4257f0d9b85f1dec68854
Ondřej Tůma <ondr@centrum.cz>
2020-02-03 10:24:58 +0100

    feature: mondodb messages

--- mongo/schemas/User.js
+++ mongo/schemas/User.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    login: {type: String, match: /[a-z\.]/i, required: true, unique: true},
+    login: {
+        type: String,
+        match: /[a-z\.]/i,
+        required: [true, 'Vyplňte jméno nebo uvidíte'],
+        unique: true
+    },
     password: {type: String, required: true},
     created: {type: Date, default: Date.now},
 });
